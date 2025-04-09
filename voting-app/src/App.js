37868fa17b67c2fcd7f179a35f5fb6dd9ee71411
nagmanikumar15876 <nagmanikumar033@gmail.com>
2025-04-09 10:04:26 +0530

    second commit

--- voting-app/src/App.js
+++ voting-app/src/App.js
@@ -21,6 +21,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         //Main hoon baap coder
+        //Main hoon bapp tera
         <Route path="/login" element={<LoginPage />} />
         <Route path="/fingerprint" element={<FingerprintScan />} />
         <Route path="/vote" element={<VoteSelection />} />
